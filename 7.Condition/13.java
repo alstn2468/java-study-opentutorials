a18d40dd27c708c0f2654729918d2f51a24cdb8c
alstn2468 <alstn2468_@naver.com>
2017-12-12 19:55:23 +0900

    2017-12-12

--- 7.Condition/13.java
+++ 7.Condition/13.java
@@ -7,30 +7,39 @@ public class Condition {
 		switch(1) {
 		case 1:
 			System.out.println("one");
+			break;
 		case 2:
 			System.out.println("two");
+			break;
 		case 3:
 			System.out.println("three");
+			break;
 		}
 
 		System.out.println("switch(2)");
 		switch(2) {
 		case 1:
 			System.out.println("one");
+			break;
 		case 2:
 			System.out.println("two");
+			break;
 		case 3:
 			System.out.println("three");
+			break;
 		}
 
 		System.out.println("switch(3)");
 		switch(3) {
 		case 1:
 			System.out.println("one");
+			break;
 		case 2:
 			System.out.println("two");
+			break;
 		case 3:
 			System.out.println("three");
+			break;
 		}
 	}
 
---
+++ 7.Condition/14.java
@@ -0,0 +1,16 @@
+package java_tutorials;
+
+public class Condition {
+
+	public static void main(String[] args) {
+		int val = 1;
+		if(val == 1) {
+			System.out.println("one");
+		}else if(val == 2) {
+			System.out.println("two");
+		}else if(val == 3) {
+			System.out.println("three");
+		}
+	}
+
+}
